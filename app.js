9771dffa926329801315413a834e07c3487f8627
Souchard <bettina.souchard@gmail.com>
2021-05-19 10:26:48 +0200

    css as usual

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var hbs = require('hbs')
 var indexRouter = require('./routes/index.routes');
 var usersRouter = require('./routes/users.routes');
 var dashboardRouter = require('./routes/dashboard.routes')
-var authRouter = require('./routes/auth.routes')
+// var authRouter = require('./routes/auth.routes')
 
 var app = express();
 
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/', dashboardRouter);
-app.use('/index', authRouter)
+// app.use('/index', authRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
