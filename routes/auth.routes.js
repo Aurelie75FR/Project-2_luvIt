4f5ffafceb6e0dc5b2b2d46eaf6e8793860e06e8
Aurelie75FR <aurelielo.vi@gmail.com>
2021-05-19 12:19:18 +0200

    authroute to test later

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,68 @@ const router = new express.Router()
 const bcrypt = require('bcrypt')
 const User = require('../models/model.user')
 
-//GET to log in  + signup
+//GET to log in 
+router.get("/signin", (req,res,next)=>{
+    res.render("index")
+})
+//GET to signup
+router.get("/signup", (req,res,next)=>{
+    res.render("signup")
+})
+//GET log out redirect homepage
+router.get("/signout", (req,res,next)=>{
+    req.session.destroy(function(err){
+        res.redirect("/index")
+    })
+})
+//POST login homepage
+router.post("/signin", async(req,res,next)=>{
+    const {email, password} = req.body
+    const foundUser =await User.findOne({email:email})
+
+    if(!foundUser){
+        req.flash("error", "Wrong Email")
+        res.redirect("/index")
+    } else{
+        const isSamePassword =bcrypt.compareSync(password, foundUser.password)
+        if(!isSamePassword){
+            req.flash("error", "Wrong Password")
+            res.redirect("/index")
+        } else{
+            const userObject = foundUser.toObject()
+            delete userObject.password
+            req.session.currentUser = userObject
+            req.flash("success", "Welcome!")
+            res.redirect("/dashboard")
+        }
+    }
+})
+//POST signup redirect to homepage 
+router.post("/signup", async (req, res, next) => {
+    try {
+      const newUser = { ...req.body };
+      const foundUser = await User.findOne({ email: newUser.email });
+  
+      if (foundUser) {
+        req.flash("warning", "email already registered");
+        res.redirect("/signup");
+      } else {
+        const hashedPassword = bcrypt.hashSync(newUser.password, 10);
+        newUser.password = hashedPassword;
+        await User.create(newUser);
+        req.flash("success", "Congrats! You can log in");
+        res.redirect("/index");
+      }
+    } catch (err) {
+      var errorMsg = "";
+      for (field in err.errors) {
+        errorMsg += err.errors[field].message + "\n";
+      }
+      req.flash("error", errorMsg);
+      res.redirect("/signup");
+    }
+  });
+
+
 
 module.exports = router;
