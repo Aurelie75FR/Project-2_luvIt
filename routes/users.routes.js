f1f23fdc9f83010ec60a7626a95ffdce55daba69
Antoine <antoinesune@gmail.com>
2021-05-20 15:17:34 +0200

    change user finished

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -10,6 +10,25 @@ router.get("/user", function (req, res, next) {
   res.render("user", {actualUser});
 });
 
+router.post("/user/:id", (req, res, next) => {
+  const editedUser = { ...req.body };
+  console.log(req.params.id)
+  UserModel.findByIdAndUpdate(req.params.id, editedUser, {new: true})
+    .then((user) => {
+      req.session.currentUser = user;
+      res.redirect("/user")
+    })
+    .catch(next);
+});
+
+router.get("/signout", async (req, res, next) => {
+  req.session.destroy(function (err) {
+    // cannot access session here anymore
+    // console.log(req.session.currentUser);
+    res.redirect("/");
+  });
+});
+
 // router.get('/userprofil', function(req, res, next) {
 //   res.render('user');
 // });
