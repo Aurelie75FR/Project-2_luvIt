6f15be8c2590760b97fa35b213fc041464af6112
Antoine <antoinesune@gmail.com>
2021-05-20 17:57:25 +0200

    en vrai cette fois c'est VRAIMENT LA DERNIERE

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/index', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/index', function(req, res, next) {
+  res.redirect('/index', { title: 'Express' });
 });
 
 module.exports = router;
